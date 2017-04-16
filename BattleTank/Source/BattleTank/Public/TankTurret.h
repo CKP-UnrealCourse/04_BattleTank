86c6f1bc57c7b83920ab3a3f59474cf3631c6dc3
Craig <ckpedersen@live.com.au>
2017-04-16 22:40:27 +1000

    141 Turret Rotation Pt.2

--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -12,8 +12,14 @@ UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
-	
-	
+
+public:
+	// -1 is max Downward speed, and +1 is max up movement
+	void Rotate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 25;
+
 	
 };
