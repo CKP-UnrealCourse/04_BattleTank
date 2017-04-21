1190fc93e6ac61bdc58b96d4c6a68495cbba71cf
Craig <ckpedersen@live.com.au>
2017-04-21 13:33:03 +1000

    155 A better Component Architecture

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -8,16 +8,6 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Possesing Tank"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possessing %s"), *(ControlledTank->GetName()));
-	}	
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
